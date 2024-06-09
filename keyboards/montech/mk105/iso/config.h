b0d0586e76e04611a340837bf3d141953f894ce9
LennartF22 <18723691+LennartF22@users.noreply.github.com>
2024-06-09 04:57:19 +0200

    Add Montech MK105

---
+++ keyboards/montech/mk105/iso/config.h
@@ -0,0 +1,25 @@
+// Copyright 2024 LennartF22 (@LennartF22)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+
+#define RGB_MATRIX_LED_COUNT 105
+
+#define SN32_PWM_DIRECTION COL2ROW
+#define SN32_RGB_MATRIX_ROW_PINS { C3, C1, C0, C6, C5, C4, C9, C8, C7, C12, C11, C10, B8, B7, B6, B11, B10, B9 }
---
+++ keyboards/montech/mk105/todo.md
@@ -0,0 +1,27 @@
+### Indicators
+
+- "M" indicator: `B12`
+- "G" indicator: `B14`
+
+### Encoder LEDs
+
+Encoder position:
+
+- X: 224
+- Y: 0
+
+Matrix locations:
+
+- East: `[5, 11]`
+- SE: `[5, 12]`
+- South: `[5, 13]`
+- SW: `[5, 17]`
+- West: `[0, 17]`
+- NW: `[0, 18]`
+- North: `[0, 19]`
+- NE: `[0, 20]`
+
+### Bootmagic Lite
+
+Does not seem to work, even when it's enabled in `info.json`,
+probably caused by shared matrix?
